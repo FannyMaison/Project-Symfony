0c7ee0a8798630701272bd5116074cc2833e497d
FannyMaison <maison1fanny@gmail.com>
2021-01-11 08:27:42 +0100

    TD 2 initial

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloWorldController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**controller HelloWorld, affiche la chaine "Hello $prenom" dans le navigateur
+     * @param string $prenom
+     * @return Response
+     */
+    function hello($prenom):Response
+    {
+        #$returnString = "Hello $prenom";
+        return $this->render('hello.html.twig', [
+            'prenom' =>$prenom
+        ]);
+    }
+
+    /**
+     * Affiche un formulaire
+     * @return Response
+     */
+    function form():Response
+    {
+        return new Response("
+        <html>
+            <body>
+            <form method='post'>
+            Nom : <input name='name'/>
+            <input type='submit'/>
+            </form>
+            </body>");
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    function formReceive(Request $request):Response
+    {
+        $formData = $request->request->get('name');
+        return new Response("Merci $formData");
+    }
+}
